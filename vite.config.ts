2e8f10cda08d7c037f4b36257ee330dc51b3e889
chensd <chensd@users.noreply.github.com>
2026-06-11 14:56:58 +0800

    fix: change base path to / for Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/lostark-arkgrid-gem-locator-v2/',
+  base: '/',
   worker: {
     format: 'es',
     // plugins: () => [svelte()],
